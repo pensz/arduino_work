850666b8818303fba4d2495c181aab0cf61afb26
孙志文 <zhiwensun@anjuke.com>
2012-12-29 14:36:36 +0800

    remove test info

--- SerialTest/SerialTest.ino
+++ SerialTest/SerialTest.ino
@@ -1,33 +1,32 @@
 void setup() {
-  Serial.begin(9600);
+    Serial.begin(9600);
 }
 
 char bufs[256];
 int i = 0;
 
 void loop() {
-  unsigned char temp;
+    unsigned char temp;
 
-  //Serial.println("begin");
-  //if (Serial.available() > 0) {
+    //if (Serial.available() > 0) {
     temp = Serial.read();
-    
-    
-    if (temp != 255) {
-      
-      if (temp == '_') {
-        bufs[i] = '\0';
 
-        Serial.print("received ");
-        Serial.println(i);
-        
-        Serial.println(bufs);        
-        i=0;
-      } else {
-        bufs[i++] = temp;
-      }
+    if (temp != 255) {
+
+        if (temp == '_') {
+            bufs[i] = '\0';
+
+            Serial.print("reply: ");
+            Serial.println(i);
+
+            Serial.println(bufs);        
+            i=0;
+        } else {
+            bufs[i++] = temp;
+        }
     }
-    delay(100);
-  //}
+
+    delay(500);
+    //}
 
 }
--- SerialTest/writeread.sh
+++ SerialTest/writeread.sh
@@ -6,23 +6,16 @@ ARDUINO_SPEED=9600
 # Set speed for usb
 stty -F $ARDUINO_PORT ispeed $ARDUINO_SPEED ospeed $ARDUINO_SPEED
 
-
-
 # Redirect Arduino port to file descriptor 6 for reading later
 exec 6<$ARDUINO_PORT
 
-echo "haha"
 # Read data from Arduino via file descriptor 6
-
-
-
-
 
 while true #
 do
     sleep 2
 
-    echo -n "abc_" > $ARDUINO_PORT 
+    echo -ne "a\nbc_" > $ARDUINO_PORT 
 
     while true
     do
@@ -34,7 +27,6 @@ do
             break
         fi
     done
-
 
     # Remove trailing carriage return character
     f=${f:0:${#f} - 1}
